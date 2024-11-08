39913eddaf962e49fb4e771597c9a9b5509c7ca0
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-11-08 20:31:23 +0900

    refactor(BaseProduct): BaseProduct로 네이밍 변경
    로직도 포함되어 있는 클래스이기 때문에 모호한 네이밍을 피하기 위해 BaseProduct로 네이밍을 변경하였습니다.
    상품 관련 로직을 구현 후 서브 클래스에 상속하기 위해 추상 클래스 키워드를 제거하였습니다.

--- src/main/java/store/domain/products/BaseProduct.java
+++ src/main/java/store/domain/products/BaseProduct.java
@@ -3,33 +3,38 @@ package store.domain.products;
 import store.global.exception.ConvenienceStoreException;
 import store.global.exception.ErrorMessage;
 
-public sealed abstract class ProductProperty permits Product, PromotionProduct {
+public sealed class BaseProduct permits Product, PromotionProduct {
 
     protected final String name;
     protected final int price;
     protected int stockQuantity;
 
-    public ProductProperty(String name, int price, int stockQuantity) {
+    public BaseProduct(String name, int price, int stockQuantity) {
         this.name = name;
         this.price = price;
         this.stockQuantity = stockQuantity;
     }
 
-    public abstract void purchase(int quantity);
-    public abstract boolean canPurchase(int quantity);
+    public void purchase(int quantity) {
+        decreaseStock(quantity);
+    }
 
-    protected void decreaseStock(int quantity) {
+    private void decreaseStock(int quantity) {
         if (!canPurchase(quantity)) {
             throw ConvenienceStoreException.from(ErrorMessage.OUT_OF_STOCK);
         }
         this.stockQuantity -= quantity;
     }
 
-    protected String getName() {
+    private boolean canPurchase(int quantity) {
+        return stockQuantity >= quantity;
+    }
+
+    public String getName() {
         return name;
     }
 
-    protected int getStockQuantity() {
+    private int getStockQuantity() {
         return stockQuantity;
     }
 }
