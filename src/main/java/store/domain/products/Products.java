e902534430eebcca1163a941e47424db3922ab7b
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-11-09 16:22:51 +0900

    feat(Products): 상품 구매 로직 추가
    프로모션 상품이 존재할 경우 프로모션 상품을 우선적으로 구매할 수 있게 하였습니다.

--- src/main/java/store/domain/products/Products.java
+++ src/main/java/store/domain/products/Products.java
@@ -4,16 +4,43 @@ import store.global.exception.ConvenienceStoreException;
 import store.global.exception.ErrorMessage;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Products {
 
     private static final List<BaseProduct> products = new ArrayList<>();
+    private static final String ORDER_PRODUCT_REGEX = "[\\[\\]]";
+    private static final String BLANK = "";
+    private static final String PRODUCT_DELIMITER = ",";
+    private static final String EACH_PRODUCT_DELIMITER = "-";
+    private static final int PRODUCT_NAME_INDEX = 0;
+    private static final int QUANTITY_INDEX = 1;
 
-    public static void purchase(String productName, int quantity) {
+    public static void purchaseProducts(String input) {
+        Arrays.stream(input.replaceAll(ORDER_PRODUCT_REGEX, BLANK).split(PRODUCT_DELIMITER))
+                .map(str -> str.split(EACH_PRODUCT_DELIMITER))
+                .forEach(parts -> purchase(
+                        parts[PRODUCT_NAME_INDEX],
+                        Integer.parseInt(parts[QUANTITY_INDEX])
+                ));
+    }
+
+    private static void purchase(String productName, int quantity) {
         BaseProduct product = findByName(productName);
+        if (product instanceof PromotionProduct) {
+            purchasePromotionProductFirst((PromotionProduct) product, quantity);
+            return;
+        }
         product.purchase(quantity);
+    }
+
+    private static void purchasePromotionProductFirst(PromotionProduct product, int quantity) {
+        int availableStock = product.stockQuantity;
+        product.purchase(availableStock);
+        findNormalProductByName(product.getName())
+                .purchase(quantity - availableStock);
     }
 
     public static List<BaseProduct> findAll() {
@@ -22,6 +49,14 @@ public class Products {
 
     public static BaseProduct findByName(String name) {
         return products.stream()
+                .filter(product -> product.getName().equals(name))
+                .findFirst()
+                .orElseThrow(() -> ConvenienceStoreException.from(ErrorMessage.PRODUCT_NOT_FOUND));
+    }
+
+    public static Product findNormalProductByName(String name) {
+        return (Product) products.reversed()
+                .stream()
                 .filter(product -> product.getName().equals(name))
                 .findFirst()
                 .orElseThrow(() -> ConvenienceStoreException.from(ErrorMessage.PRODUCT_NOT_FOUND));
