db25a7e9e394b4575dd1344f4a4ed8982c47a464
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-11-09 16:20:24 +0900

    feat(PromotionProduct): 프로모션 상품 구매 메서드 추가

--- src/main/java/store/domain/products/PromotionProduct.java
+++ src/main/java/store/domain/products/PromotionProduct.java
@@ -5,6 +5,9 @@ import store.domain.events.Promotions;
 
 public final class PromotionProduct extends BaseProduct {
 
+    private static final int OUT_OF_STOCK_QUANTITY = 0;
+
+
     private final Promotion promotion;
 
     public PromotionProduct(String name, int price, int stockQuantity, String promotionName) {
@@ -14,5 +17,14 @@ public final class PromotionProduct extends BaseProduct {
 
     public String getPromotionName() {
         return promotion.getName();
+    }
+
+    @Override
+    public void purchase(int quantity) {
+        if (isOutOfStock(quantity)) {
+            stockQuantity = OUT_OF_STOCK_QUANTITY;
+            return;
+        }
+        this.stockQuantity -= quantity;
     }
 }
