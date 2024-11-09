7ae5396e9914747c33bb6c51253fce93b523ec67
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-11-09 16:20:09 +0900

    feat(Product): 일반 상품 구매 메서드 추가

--- src/main/java/store/domain/products/Product.java
+++ src/main/java/store/domain/products/Product.java
@@ -1,8 +1,19 @@
 package store.domain.products;
 
+import store.global.exception.ConvenienceStoreException;
+import store.global.exception.ErrorMessage;
+
 public final class Product extends BaseProduct {
 
     public Product(String name, int price, int stockQuantity) {
         super(name, price, stockQuantity);
+    }
+
+    @Override
+    public void purchase(int quantity) {
+        if (isOutOfStock(quantity)) {
+            throw ConvenienceStoreException.from(ErrorMessage.OUT_OF_STOCK);
+        }
+        this.stockQuantity -= quantity;
     }
 }
