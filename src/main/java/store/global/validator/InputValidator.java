6d1a49ba702a99e4134ffe173aa2a3fad88fe6b9
JaeHyeon Yoo <jhyoo1203@gmail.com>
2024-11-10 18:26:31 +0900

    feat(InputValidator): 입력 검증 클래스 추가
    상품 입력에 대한 검증 메서드를 추가하였습니다.
    Y 또는 N 입력에 대한 검증 메서드를 추가하였습니다.

---
+++ src/main/java/store/global/validator/InputValidator.java
@@ -0,0 +1,36 @@
+package store.global.validator;
+
+import store.global.exception.ConvenienceStoreException;
+import store.global.exception.ErrorMessage;
+
+import java.util.regex.Pattern;
+
+public class InputValidator {
+
+    private static final String PRODUCT_FORMAT_REGEX = "^\\[([^-]+-\\d+)](,\\[([^-]+-\\d+)])*$";
+    private static final String YES_NO_REGEX = "^[YN]$";
+
+    public static void validateProductInput(String input) {
+        validateEmpty(input);
+        validateProductFormat(input);
+    }
+
+    private static void validateEmpty(String input) {
+        if (input == null || input.trim().isEmpty()) {
+            throw ConvenienceStoreException.from(ErrorMessage.INVALID_INPUT_ERROR);
+        }
+    }
+
+    private static void validateProductFormat(String input) {
+        if (!Pattern.matches(PRODUCT_FORMAT_REGEX, input)) {
+            throw ConvenienceStoreException.from(ErrorMessage.INVALID_INPUT_ERROR);
+        }
+    }
+
+    public static void validateYesNoInput(String input) {
+        validateEmpty(input);
+        if (!Pattern.matches(YES_NO_REGEX, input)) {
+            throw ConvenienceStoreException.from(ErrorMessage.INVALID_INPUT_ERROR);
+        }
+    }
+}
